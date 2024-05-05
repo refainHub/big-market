adbb1b163509b1277b7eea7be49b1026dbfc96ef
refain <3074622675@qq.com>
2024-05-06 01:55:30 +0800

    策略权重概率装配v2

--- big-market-domain/src/main/java/cn/bugstack/domain/strategy/repository/IStrategyRepository.java
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/repository/IStrategyRepository.java
@@ -1,7 +1,10 @@
 package cn.bugstack.domain.strategy.repository;
 
 
+
 import cn.bugstack.domain.strategy.model.entity.StrategyAwardEntity;
+import cn.bugstack.domain.strategy.model.entity.StrategyEntity;
+import cn.bugstack.domain.strategy.model.entity.StrategyRuleEntity;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -15,10 +18,17 @@ public interface IStrategyRepository {
 
     List<StrategyAwardEntity> queryStrategyAwardList(Long strategyId);
 
-    void storeStrategyAwardSearchRateTable(Long strategyId, Integer rateRange, Map<Integer, Integer> strategyAwardSearchRateTable);
+    void storeStrategyAwardSearchRateTable(String key, Integer rateRange, Map<Integer, Integer> strategyAwardSearchRateTable);
 
-    Integer getStrategyAwardAssemble(Long strategyId, Integer rateKey);
+    Integer getStrategyAwardAssemble(String key, Integer rateKey);
 
     int getRateRange(Long strategyId);
 
+    int getRateRange(String key);
+
+    StrategyEntity queryStrategyEntityByStrategyId(Long strategyId);
+
+    StrategyRuleEntity queryStrategyRule(Long strategyId, String ruleModel);
+
 }
+
